83d038a885c53ac4d0660c1a07992c45537e5daa
堀涼 <horiryou@ryo.local>
2022-08-21 19:28:51 +0900

    slack

--- backend/slack.py
+++ backend/slack.py
@@ -4,18 +4,46 @@ class Slack:
     def __init__(self, token='xoxb-3651744076246-3968426113411-sGuvyQURzhNeGXXA4auzKVv0', channel_id='C03U9T9T7C6'):
         self.channel_id = channel_id
         self.client = WebClient(token=token)
+        self.users = list(list())
+        self.replies = list()
 
-    def send_msg(self, msg):
-        self.client.chat_postMessage(channel=self.channel_id, text=msg)
-        return True
+    def _send_msg(self, id_, msg):
+        self.client.chat_postMessage(channel=id_, text=msg)
 
-    def send_img(self, img):
-        self.client.files_upload(channels=self.channel_id, file=img)
-        return True
+    def _send_img(self, id_, img_path):
+        self.client.files_upload(channels=id_, file=img_path)
+
+    def get_users(self):
+        users = self.client.users_list()['members']
+        for user in users:
+            print(user)
+            is_bot = user['is_bot']
+            if not is_bot:
+                self.users.append((user['id'], user['name']))
+
+    def send2users(self, img_path):
+        for user_id, user_name in self.users:
+            print(user_id, user_name)
+            if user_name == 'slackbot':
+                continue
+            self._send_msg(user_id, 'Hello ' + user_name)
+            self._send_img(user_id, img_path)
+
+    def _get_ims(self):
+        return self.client.conversations_list(types='im')['channels']
+        
+    def get_latest_reply(self):
+        for im in self._get_ims():
+            id_ = im['id']
+            latest_message = self.client.conversations_history(channel=id_, limit=1)['messages']
+            ts = latest_message[0]['ts']
+            reply = self.client.conversations_replies(channel=id_, ts=ts, limit=5)['messages'][-1]['text']
+            self.replies.append(reply)
 
 if __name__ == '__main__':
     token = 'xoxb-3651744076246-3968426113411-sGuvyQURzhNeGXXA4auzKVv0'
     channel_id = 'C03U9T9T7C6'
     slack = Slack(token, channel_id)
-    slack.send_msg('Hello World')
-    slack.send_img('./test.jpeg')
+    slack.get_users()
+    #slack.send2users('./test.jpeg')
+    slack.get_latest_reply()
